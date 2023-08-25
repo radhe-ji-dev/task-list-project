e27e2d3e578feceb20c6d243cbae2e68b0f73b44
Radhakant Tiwari <therktiwadi@gmail.com>
2023-08-25 09:43:18 +0530

    initial setup and routes

---
+++ task-list-project/routes/web.php
@@ -0,0 +1,128 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    
+    class Task
+    {
+        public int $id;
+        public string $title;
+        public string $description;
+        public ?string $long_description;
+        public bool $completed;
+        public string $created_at;
+        public string $updated_at;
+    
+        public function __construct(
+            int $id,
+            string $title,
+            string $description,
+            ?string $long_description,
+            bool $completed,
+            string $created_at,
+            string $updated_at
+        ) {
+            $this->id = $id;
+            $this->title = $title;
+            $this->description = $description;
+            $this->long_description = $long_description;
+            $this->completed = $completed;
+            $this->created_at = $created_at;
+            $this->updated_at = $updated_at;
+        }
+    }
+    
+    $tasks = [
+        new Task(
+            1,
+            'Buy groceries',
+            'Task 1 description',
+            'Task 1 long description',
+            false,
+            '2023-03-01 12:00:00',
+            '2023-03-01 12:00:00'
+        ),
+        new Task(
+            2,
+            'Sell old stuff',
+            'Task 2 description',
+            null,
+            false,
+            '2023-03-02 12:00:00',
+            '2023-03-02 12:00:00'
+        ),
+        new Task(
+            3,
+            'Learn programming',
+            'Task 3 description',
+            'Task 3 long description',
+            true,
+            '2023-03-03 12:00:00',
+            '2023-03-03 12:00:00'
+        ),
+        new Task(
+            4,
+            'Take dogs for a walk',
+            'Task 4 description',
+            null,
+            false,
+            '2023-03-04 12:00:00',
+            '2023-03-04 12:00:00'
+        ),
+    ];
+});
+
+    Route::get('/', function () use ($tasks) {
+        return view('index', [
+            'name' => 'Piotr',
+            'tasks' => $tasks
+        ]);
+    })->name('tasks.index');
+
+    
+    Route::get('/xxx', function () {
+        return 'Hello';
+    })->name('hello');
+    Route::get('/{id}', function ($id) {
+        return 'One single task';
+    })->name('tasks.show');
+    
+    Route::get('/hallo', function () {
+        return redirect()->route('hello');
+    });
+    // Route::get('/xxx', function () {
+    //     return 'Hello';
+    // })->name('hello');
+    
+    Route::get('/greet/{name}', function ($name) {
+        return 'Hello ' . $name . '!';
+    });
+    // Route::get('/hallo', function () {
+    //     return redirect()->route('hello');
+    // });
+    
+    // Route::get('/greet/{name}', function ($name) {
+    //     return 'Hello ' . $name . '!';
+    // });
+    
+    Route::fallback(function () {
+        return 'Still got somewhere!';
+    });
+    
+    // GET
+    // POST 
+    // PUT 
+    // DELETE 
+    
