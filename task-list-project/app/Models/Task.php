9df581b3aeed8fe5c83ea7879f10bcb94c0d9e86
Radhakant Tiwari <therktiwadi@gmail.com>
2023-08-28 08:34:56 +0530

    model query

--- task-list-project/app/Models/Task.php
+++ task-list-project/app/Models/Task.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'long_description',
+        'completed',
+        'created_at',
+        'updated_at'
+    ];
 }
